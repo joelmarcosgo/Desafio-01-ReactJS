6f440e361ddbf88b09ff9318b14f05badbdb70b6
joelmarcosgo <marcos@gtcommerce.com.br>
2021-08-26 15:57:27 -0300

    :todo: Completed Task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const createNewTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTask => [...oldTask, createNewTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const markAsFinished = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task)
+
+    setTasks(markAsFinished)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const removeTask = tasks.filter(task => task.id !== id)
+
+    setTasks(removeTask)
   }
 
   return (
